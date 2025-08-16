07aeb3c5ecf5cd789a3ae5a93e383aebb2f7e22e
haymi-merry <emutiyeee1996@gmail.com>
2025-08-16 17:36:49 +0300

    Hero shadow

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,11 +3,10 @@ import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <section className="relative w-full  bg-gray-50 pt-12 pb-16">
+    <section className="relative w-full bg-gray-50 pt-12 pb-16 overflow-hidden">
       <div className="relative z-10 w-full h-full flex flex-col md:flex-row items-center justify-center px-10">
         
-        {/* Left: Text content */}
-        <div className="text-center md:text-left max-w-2xl space-y-6 md:mr-12 flex-1">
+        <div className="text-center md:text-left max-w-2xl space-y-6 md:mr-12 ml-36 flex-1">
           <h1 className="text-4xl md:text-6xl font-bold leading-tight">
             <span className="block text-gray-800">Faith Meets Code</span>
             <span className="block text-[#721111]">Culture Meets Innovation</span>
@@ -19,26 +18,23 @@ export default function Hero() {
           
           <Link 
             href="#"
-            className="mt-8 bg-[#721111] hover:bg-[#5a0d0d] text-white font-bold py-3 px-8 rounded-full transition-colors"
+            className="mt-8 bg-[#721111] hover:bg-[#5a0d0d] text-white font-bold py-3 px-8 rounded-full transition-colors inline-block"
           >
             Join our Telegram Community
           </Link>
         </div>
 
-        {/* Right: Church Image above Red Frame */}
         <div className="relative flex-1 flex justify-center items-center mb-8 md:mb-0">
-          {/* Red frame */}
           <div 
             className="absolute bg-[#721111] z-0"
             style={{
-              width: '1019px',
-              height: '361px',
+              width: '700px',
+              height: '250px',
               top: '50%', 
               left: '50%', 
               transform: 'translate(-50%, -50%) rotate(90deg)',
             }}
           ></div>
-          {/* Church Image */}
           <div className="relative z-10">
             <Image 
               src="/EOTC_church.png" 
@@ -50,6 +46,12 @@ export default function Hero() {
           </div>
         </div>
       </div>
+      <div
+        className="absolute left-0 bottom-0 w-full h-24 pointer-events-none"
+        style={{
+          background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.2) 4.08%, #FFFFFF 14.95%)',
+        }}
+      />
     </section>
-    );
+  );
 }
